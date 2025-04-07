cb20d16ab2c2d9b88afed618de3c3a1dd554f396
Briana_ML <brianamikk2@gamil.com>
2025-04-07 17:55:05 -0300

    databse lista by clem y bri

--- db.sql
+++ db.sql
@@ -40,5 +40,36 @@ CREATE TABLE comentarios (
 
 -- 5 usuarios, 10 posteos, 3 comentarios (per posteo) > modulo de datos
 
--- Los campos de las tablas deben coincidir con la información solicitada en cada formulario creado para el proyecto. 
--- Debe existir coherencia entre los campos pedidos en el formulario y los datos que almacena la tabla.
+INSERT INTO usuarios(id, email, contrasenia, fecha_nacimiento, dni, foto_perfil, createDat, updatedAt, deletedAt)
+VALUES 
+(DEFAULT, "cdechazal@udesa.edu.ar", "12345678", "2006-02-04", 46051577, "fotot0.jpg", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL),
+(DEFAULT, "juan.perez@mail.com", "claveSegura1", "2000-05-10", 40123456, "foto1.jpg", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL),
+(DEFAULT, "maria.lopez@mail.com", "password2024", "1998-11-23", 39234567, "foto2.png", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL),
+(DEFAULT, "lucas.gomez@mail.com", "lucasPass9", "2002-07-15", 41234567, "foto3.jpeg", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL),
+(DEFAULT, "sofia.ramirez@mail.com", "sofi4Life", "1995-03-28", 38234567, "foto4.jpg", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL);
+
+INSERT INTO productos(id, img_producto, nombre, descripcion, createDat, updatedAt, deletedAt, usuario_id)
+VALUES
+(DEFAULT, "superstar.jpg", "Adidas Superstar", "Zapatillas cómodas y a la moda", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 1),
+(DEFAULT, "airmax90.jpg", "Nike Air Max 90", "Zapatillas clásicas con gran amortiguación", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 2),
+(DEFAULT, "conversehi.jpg", "Converse Chuck Taylor High", "Estilo retro para todos los días", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 1),
+(DEFAULT, "pumarsuede.jpg", "Puma Suede Classic", "Diseño urbano con un toque vintage", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 2),
+(DEFAULT, "nb574.jpg", "New Balance 574", "Comodidad y estilo en cada paso", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 2),
+(DEFAULT, "reebokclubc.jpg", "Reebok Club C 85", "Elegancia casual para uso diario", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 2),
+(DEFAULT, "vansoldskool.jpg", "Vans Old Skool", "Perfectas para skaters y fans del streetwear", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 5),
+(DEFAULT, "jordan1.jpg", "Air Jordan 1 Retro", "Icónicas zapatillas de básquet y cultura urbana", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 5),
+(DEFAULT, "asicsgel.jpg", "Asics Gel-Lyte III", "Tecnología de amortiguación y diseño único", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 5),
+(DEFAULT, "fila disruptor.jpg", "Fila Disruptor II", "Estilo chunky con mucha personalidad", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 5);
+
+INSERT INTO comentarios(id, texto, createDat, updatedAt, deletedAt, usuario_id, producto_id)
+VALUES
+(DEFAULT, "Estas zapas me salvan siempre", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 4, 1),
+(DEFAULT, "¡Son muy cómodas para correr!", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 5, 2),
+(DEFAULT, "Clásico que nunca falla.", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 3, 3),
+(DEFAULT, "Las uso todo el tiempo. Re durables.", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 2, 4),
+(DEFAULT, "¡Perfectas para caminar por la ciudad!", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 1, 5),
+(DEFAULT, "Me encantan con jeans claros.", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 4, 6),
+(DEFAULT, "Re cómodas y combinan con todo.", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 5, 7),
+(DEFAULT, "Son una joya. Me las pongo solo en ocasiones especiales.", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 2, 8),
+(DEFAULT, "Geniales para el gimnasio.", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 3, 9),
+(DEFAULT, "Me encanta el diseño grueso, son distintas a todo.", '2025-04-07 12:00:00', '2025-04-07 12:00:00', NULL, 1, 10);
