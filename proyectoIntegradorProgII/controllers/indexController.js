1a2dd0920e13b5bcb3b4a6de716354c51cd8e2c2
clemendechazal <cdechazal@udesa.edu.ar>
2025-04-08 13:49:05 -0300

    intentando hacer el punto 1

--- proyectoIntegradorProgII/controllers/indexController.js
+++ proyectoIntegradorProgII/controllers/indexController.js
@@ -2,7 +2,7 @@ const data= require("../db/index")
 
 const controller={
     index: function (req, res) {
-        return res.render("index")
+        return res.render("index",{title: "Zapelle"})
     }
 }
 
