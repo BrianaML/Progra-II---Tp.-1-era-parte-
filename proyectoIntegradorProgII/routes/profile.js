36de25ade362ee111a0dd1ce31b3f140d2f8b8bc
CatalinaMarcos <cmarcosaslanian@udesa.edu.ar>
2025-04-08 22:34:38 -0300

    arregle las rutas para q anden las paginas

--- proyectoIntegradorProgII/routes/profile.js
+++ proyectoIntegradorProgII/routes/profile.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const profileController = require('../controllers/profileController');
-router.get('/profile', profileController.profile);
+router.get('/', profileController.profile);
 
 module.exports = router;
--- proyectoIntegradorProgII/routes/register.js
+++ proyectoIntegradorProgII/routes/register.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const registerController = require('../controllers/registerController');
-router.get('/register', registerController.register);
+router.get('/', registerController.register);
 
 module.exports = router;
