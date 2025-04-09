a4016f42630f14e84555ebc9d3f77acf7dab5d0b
Briana_ML <brianamikk2@gamil.com>
2025-04-09 17:16:20 -0300

    editando el product

--- proyectoIntegradorProgII/db/index.js
+++ proyectoIntegradorProgII/db/index.js
@@ -43,222 +43,222 @@ let data = {
     ],
     productos: [
         {
-            imagen: "Superstar.avif",
+            imagen: "/images/products/Superstar.avif",
             nombre: "Adidas Superstar",
             descripcion: "Zapatillas cómodas y a la moda",
             comentarios: [
                 {
                     nombre_usuario: "lucas.gomez",
                     texto: "Estas zapas me salvan siempre",
-                    imagen_perfil: "foto3.jpeg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "sofia.ramirez",
                     texto: "Muy buena calidad y super cómodas.",
-                    imagen_perfil: "foto4.jpg"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "maria.lopez",
                     texto: "Son mi primera opción siempre.",
-                    imagen_perfil: "foto2.png"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "NikeAirMax90.avif",
+            imagen: "/images/products/NikeAirMax90.avif",
             nombre: "Nike Air Max 90",
             descripcion: "Zapatillas clásicas con gran amortiguación",
             comentarios: [
                 {
                     nombre_usuario: "sofia.ramirez",
                     texto: "¡Son muy cómodas para correr!",
-                    imagen_perfil: "foto4.jpg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "juan.perez",
                     texto: "No me hicieron doler nunca los pies.",
-                    imagen_perfil: "foto1.jpg"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "cdechazal",
                     texto: "Perfectas para entrenar.",
-                    imagen_perfil: "fotot0.jpg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "converse.jpg",
+            imagen: "/images/products/converse.jpg",
             nombre: "Converse Chuck Taylor High",
             descripcion: "Estilo retro para todos los días",
             comentarios: [
                 {
                     nombre_usuario: "maria.lopez",
                     texto: "Clásico que nunca falla.",
-                    imagen_perfil: "foto2.png"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "lucas.gomez",
                     texto: "Siempre quise tener unas así.",
-                    imagen_perfil: "foto3.jpeg"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "cdechazal",
                     texto: "Me gusta mucho el diseño retro.",
-                    imagen_perfil: "fotot0.jpg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "pumaRosa.webp",
+            imagen: "/images/products/pumaRosa.webp",
             nombre: "Puma Suede Classic",
             descripcion: "Diseño urbano con un toque vintage",
             comentarios: [
                 {
                     nombre_usuario: "juan.perez",
                     texto: "Las uso todo el tiempo. Re durables.",
-                    imagen_perfil: "foto1.jpg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "maria.lopez",
                     texto: "Van con cualquier look.",
-                    imagen_perfil: "foto2.png"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "sofia.ramirez",
                     texto: "Aguantan un montón el uso diario.",
-                    imagen_perfil: "foto4.jpg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "NewBalance.jpg",
+            imagen: "/images/products/NewBalance.jpg",
             nombre: "New Balance 574",
             descripcion: "Comodidad y estilo en cada paso",
             comentarios: [
                 {
                     nombre_usuario: "cdechazal",
                     texto: "¡Perfectas para caminar por la ciudad!",
-                    imagen_perfil: "fotot0.jpg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "juan.perez",
                     texto: "Livianas y super cómodas.",
-                    imagen_perfil: "foto1.jpg"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "lucas.gomez",
                     texto: "No me canso de usarlas.",
-                    imagen_perfil: "foto3.jpeg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "reebokclubc.jpg",
+            imagen: "/images/products/reebokclubc.jpg",
             nombre: "Reebok Club C 85",
             descripcion: "Elegancia casual para uso diario",
             comentarios: [
                 {
                     nombre_usuario: "lucas.gomez",
                     texto: "Me encantan con jeans claros.",
-                    imagen_perfil: "foto3.jpeg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "maria.lopez",
                     texto: "El color es lo más.",
-                    imagen_perfil: "foto2.png"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "juan.perez",
                     texto: "Van con todo, super recomendadas.",
-                    imagen_perfil: "foto1.jpg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "vansoldskool.jpg",
+            imagen: "/images/products/vansoldskool.jpg",
             nombre: "Vans Old Skool",
             descripcion: "Perfectas para skaters y fans del streetwear",
             comentarios: [
                 {
                     nombre_usuario: "sofia.ramirez",
                     texto: "Re cómodas y combinan con todo.",
-                    imagen_perfil: "foto4.jpg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "maria.lopez",
                     texto: "El diseño me encanta.",
-                    imagen_perfil: "foto2.png"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "lucas.gomez",
                     texto: "Las volvería a comprar sin dudar.",
-                    imagen_perfil: "foto3.jpeg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "jordan1.jpg",
+            imagen: "/images/products/jordan1.jpg",
             nombre: "Air Jordan 1 Retro",
             descripcion: "Icónicas zapatillas de básquet y cultura urbana",
             comentarios: [
                 {
                     nombre_usuario: "juan.perez",
                     texto: "Son una joya. Me las pongo solo en ocasiones especiales.",
-                    imagen_perfil: "foto1.jpg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "cdechazal",
                     texto: "Me gusta mucho cómo brillan.",
-                    imagen_perfil: "fotot0.jpg"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "sofia.ramirez",
                     texto: "Recibo siempre elogios cuando las uso.",
-                    imagen_perfil: "foto4.jpg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "asicsgel.jpg",
+            imagen: "/images/products/asicsgel.jpg",
             nombre: "Asics Gel-Lyte III",
             descripcion: "Tecnología de amortiguación y diseño único",
             comentarios: [
                 {
                     nombre_usuario: "maria.lopez",
                     texto: "Geniales para el gimnasio.",
-                    imagen_perfil: "foto2.png"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "lucas.gomez",
                     texto: "No resbalan nada.",
-                    imagen_perfil: "foto3.jpeg"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "juan.perez",
                     texto: "Ideales para entrenar fuerte.",
-                    imagen_perfil: "foto1.jpg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         },
         {
-            imagen: "filadisruptor.jpg",
+            imagen: "/images/products/filadisruptor.jpg",
             nombre: "Fila Disruptor II",
             descripcion: "Estilo chunky con mucha personalidad",
             comentarios: [
                 {
                     nombre_usuario: "cdechazal",
                     texto: "Me encanta el diseño grueso, son distintas a todo.",
-                    imagen_perfil: "fotot0.jpg"
+                    imagen_perfil: "/images/users/cute.jpeg"
                 },
                 {
                     nombre_usuario: "maria.lopez",
                     texto: "Siento que suman un montón de estilo.",
-                    imagen_perfil: "foto2.png"
+                    imagen_perfil: "/images/users/gato.jpeg"
                 },
                 {
                     nombre_usuario: "juan.perez",
                     texto: "Súper originales, no se ven mucho.",
-                    imagen_perfil: "foto1.jpg"
+                    imagen_perfil: "/images/users/queen.jpeg"
                 }
             ]
         }]};
