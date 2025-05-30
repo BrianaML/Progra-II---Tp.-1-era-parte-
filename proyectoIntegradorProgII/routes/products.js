0bb8cb5c0c94bcc32dbeb84344e798b708b25547
Briana_ML <brianamikk2@gamil.com>
2025-05-30 15:13:28 -0300

    cambio de lugar

--- proyectoIntegradorProgII/routes/products.js
+++ proyectoIntegradorProgII/routes/products.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 const productController = require("../controllers/productController");
 
-router.get('/:id', productController.product);
-
 router.get("/productAdd", productController.productAdd);
 router.post("/productAdd", productController.productCreate);
 
 router.get("/searchResults", productController.searchResults);
+router.get('/:id', productController.product);
 
 router.post('/agregarComentario', productController.agregarComentario);
+
 
 module.exports = router;
